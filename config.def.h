aee0b2fae8b38f599c84223c5232b74f755ebff6
NicoDevStuff <NicoG2008@gmx.de>
2023-03-21 16:56:01 +0100

    i messed upppp

--- config.def.h
+++ config.def.h
@@ -2,8 +2,9 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 12;        /* gaps between windows */
+static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
+<<<<<<< HEAD
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
@@ -25,20 +26,28 @@ static const int topbar             = 1;     /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrains Mono:size=14", 
 										"Material Icons:size=14",
 										"JoyPixels:style=Regular:size=14"
+=======
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
+static const int showbar            = 1;        /* 0 means no bar */
+static const int topbar             = 1;        /* 0 means bottom bar */
+static const char *fonts[]          = { "JetBrains Mono:size=13", 
+										"Material Icons:size=13",
+										"JoyPixels:style=Regular:size=13"
+>>>>>>> parent of 116b01a (idk wtf i did, now patching status2d)
 									  };
 static const char dmenufont[]       = "JetBrains Mono:size=13";
-static unsigned int baralpha        = 0xB4;
+static unsigned int baralpha        = 0xd0;
 static unsigned int borderalpha     = OPAQUE;
 static const char col_gray1[]       = "#282828";
-static const char col_gray2[]       = "#3c3836";
+static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#ebdbb2";
 static const char col_gray4[]       = "#fbf1c7";
 static const char col_cyan[]        = "#005577";
 static const char col_cyanbright[]  = "#83a596";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1,  col_gray2 },
-	[SchemeSel]  = { col_gray4, col_gray2,  col_cyanbright  },
+	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_gray1,  col_cyanbright  },
 };
 
 /* tagging */
