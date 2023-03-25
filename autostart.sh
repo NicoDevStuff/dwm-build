ac8fd720b9b91915ced803319543a026a26846a7
NicoDevStuff <NicoG2008@gmx.de>
2023-03-25 12:01:34 +0100

    Update autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -4,11 +4,10 @@ xbindkeys -f ~/.config/.xbindkeysrc &
 picom  &
 ~/.screenlayout/main.sh &
 udiskie &
-killall nm-applet && nm-applet &
-killall volumeicon && volumeicon &
 easyeffects &
+solaar &
 xclip &
-killall dwmblocks && dwmblocks &
+dwmblocks &
 conky --config ~/.config/conky/dwm/conky.conf & 
 nitrogen --restore &  
 
