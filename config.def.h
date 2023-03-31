94be2d185314dda1a096ca66faef5213ff982915
NicoDevStuff <NicoG2008@gmx.de>
2023-03-31 16:39:43 +0200

    patching in status2d

--- config.def.h
+++ config.def.h
@@ -22,8 +22,8 @@ static const char col_cyan[]        = "#005577";
 static const char col_cyanbright[]  = "#83a596";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_gray1, col_cyanbright  },
+	[SchemeNorm] = { col_gray4, col_gray1, col_gray2 },
+	[SchemeSel]  = { col_gray4, col_gray2, col_cyanbright  },
 };
 
 /* tagging */
